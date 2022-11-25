55ac70a787025c4d4fa5f792e21a73983cf946f5
Nigel <nigel.ong00@gmail.com>
2022-11-24 18:13:49 -0800

    added my noteArray list of objects for notes aside

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,20 +1,10 @@
-// const ThemeButton = document.querySelector(".purple_button");
-
-// function clickevent(event)
-
-// ThemeButton.addEventListener("click", changeText);
-
-// function changeText() 
-// {
-//     if (ThemeButton.textContent === "Dark Theme"){
-//         ThemeButton.textContent = "Light Theme";
-// }
-// else if(ThemeButton.textContent === "Light Theme") {
-//     ThemeButton.textContent = "Dark Theme";
-// }}
-
 const ThemeButton = document.querySelector(".button_purple");
 
+const noteArray = 
+[
+    {title:"note one", body:"this is my first note"}
+    {title:"note two", body:"this is my second note"}
+]
 
 function changeText() 
 {
@@ -25,3 +15,9 @@ function changeText()
 }
 
 ThemeButton.addEventListener("click", changeText);
+
+const noteArray = 
+[
+    {title:"note one", body:"this is my first note"}
+    {title:"note two", body:"this is my second note"
+]
